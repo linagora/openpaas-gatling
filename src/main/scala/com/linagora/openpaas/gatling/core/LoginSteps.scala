dfc736ed11fcb2946f9640625a01a7f65ccd8679
Tuan Tuan LE <tlcong@linagora.com>
2019-10-31 10:43:12 +0700

    #16 add login steps
    Load login page templates
    
    Login step

---
+++ src/main/scala/com/linagora/openpaas/gatling/core/LoginSteps.scala
@@ -0,0 +1,32 @@
+package com.linagora.openpaas.gatling.core
+
+import com.linagora.openpaas.gatling.provisionning.SessionKeys._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
+import com.linagora.openpaas.gatling.core.LoginTemplateRequestsList._
+import io.gatling.core.structure.ChainBuilder
+
+object LoginSteps {
+
+  def loadLoginTemplates: ChainBuilder =
+    group("Load login templates") {
+      repeat(loadTemplate.length, "index") {
+        exec(session => {
+          val index = session("index").as[Int]
+          val resourceURL = loadTemplate(index)
+          session.set("resourceURL", resourceURL)
+        })
+          .exec(http(s"Load $${resourceURL}")
+            .get(s"$${resourceURL}")
+            .check(status in(200, 304)))
+      }
+    }
+
+  def login(): HttpRequestBuilder =
+    http("Login")
+    .post("/api/login")
+    .body(StringBody(s"""{"username":"$${$UsernameSessionParam}","password":"$${$PasswordSessionParam}","rememberme":false,"recaptcha":{"needed":false,"data":null}}"""))
+      .check(status.is(200))
+      .check(jsonPath("$._id").saveAs(UserId))
+}
