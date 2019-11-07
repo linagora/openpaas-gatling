df2561a49af0a5a3314396ae97f8aa3c3e1235d2
Tuan Tuan LE <tlcong@linagora.com>
2019-11-07 13:23:46 +0700

    #15 define generate jwt token step

--- src/main/scala/com/linagora/openpaas/gatling/core/TokenSteps.scala
+++ src/main/scala/com/linagora/openpaas/gatling/core/TokenSteps.scala
@@ -3,7 +3,9 @@ package com.linagora.openpaas.gatling.core
 import com.linagora.openpaas.gatling.provisionning.Authentication.withAuth
 import com.linagora.openpaas.gatling.provisionning.SessionKeys._
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
 
 object TokenSteps {
   def retrieveToken =
@@ -11,4 +13,15 @@ object TokenSteps {
       .get("/api/authenticationtoken"))
       .check(status in (200, 304))
       .check(jsonPath("$.token").saveAs(Token))
+
+  def generateJwtToken: ChainBuilder =
+    exec(http("Generate jwt token")
+      .post("/api/jwt/generate")
+      .check(status is 200)
+      .check(bodyString.saveAs(JwtToken))
+    )
+    .exec(session => {
+      val denormalizedJwtToken = session(JwtToken).as[String].replaceAll("\"", "")
+      session.set(JwtToken, denormalizedJwtToken)
+    })
 }
