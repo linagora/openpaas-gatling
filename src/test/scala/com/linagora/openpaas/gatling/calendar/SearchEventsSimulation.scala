28f00a1e27c044b07bb79da499ac2fef007074ce
Christophe Hamerling <chamerling@linagora.com>
2020-02-24 22:24:34 +0000

    Add missing logout

--- src/test/scala/com/linagora/openpaas/gatling/calendar/SearchEventsSimulation.scala
+++ src/test/scala/com/linagora/openpaas/gatling/calendar/SearchEventsSimulation.scala
@@ -3,7 +3,7 @@ package com.linagora.openpaas.gatling.calendar
 import com.linagora.openpaas.gatling.Configuration._
 import com.linagora.openpaas.gatling.calendar.CalendarsSteps._
 import com.linagora.openpaas.gatling.core.UserSteps.getProfile
-import com.linagora.openpaas.gatling.core.LoginSteps.login
+import com.linagora.openpaas.gatling.core.LoginSteps._
 import com.linagora.openpaas.gatling.utils.RandomUuidGenerator.randomUuidString
 import com.linagora.openpaas.gatling.core.DomainSteps._
 import io.gatling.core.Predef._
@@ -22,6 +22,8 @@ class SearchEventsSimulation extends  Simulation{
     .exec(getProfile())
     .pause(1 second)
     .exec(provisionEvents)
+    .pause(1 second)
+    .exec(logout)
     .pause(1 second)
     .during(ScenarioDuration) {
       exec(SearchEventsScenari.generate())
