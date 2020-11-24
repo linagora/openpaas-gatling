52093c5c7cc6a2b2a654feb37d61c283b768df91
Rémi KOWALSKI <rkowalski@linagora.com>
2020-11-24 16:30:32 +0100

    use authentication to generate jwt token and to retrieve auth token

--- src/main/scala/com/linagora/openpaas/gatling/core/TokenSteps.scala
+++ src/main/scala/com/linagora/openpaas/gatling/core/TokenSteps.scala
@@ -2,29 +2,17 @@ package com.linagora.openpaas.gatling.core
 
 import com.linagora.openpaas.gatling.provisionning.SessionKeys._
 import io.gatling.core.Predef._
-import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
 import com.linagora.openpaas.gatling.provisionning.Authentication._
 
 object TokenSteps {
 
   def retrieveAuthenticationToken =
-    exec(http("Get Authentication Token")
+    exec(withAuth(http("Get Authentication Token")
       .get("/api/authenticationtoken")
       .check(status in (200, 304))
-      .check(jsonPath("$.token").saveAs(Token))
+      .check(jsonPath("$.token").saveAs(Token)))
     )
-
-  def generateJwtToken: ChainBuilder =
-    exec(http("Generate jwt token")
-      .post("/api/jwt/generate")
-      .check(status is 200)
-      .check(bodyString.saveAs(JwtToken))
-    )
-    .exec(session => {
-      val denormalizedJwtToken = session(JwtToken).as[String].replaceAll("\"", "")
-      session.set(JwtToken, denormalizedJwtToken)
-    })
 
   def generateJwtTokenWithAuth =
     exec(withAuth(http("Generate jwt token")
