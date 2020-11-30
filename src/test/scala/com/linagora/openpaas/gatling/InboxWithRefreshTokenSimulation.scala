11d35f5fc7f8f65fa2ea5076ce5445e338efb85f
Rémi KOWALSKI <rkowalski@linagora.com>
2020-11-30 09:59:52 +0100

    Rename platform simulation to inbox

--- src/test/scala/com/linagora/openpaas/gatling/InboxWithRefreshTokenSimulation.scala
+++ src/test/scala/com/linagora/openpaas/gatling/InboxWithRefreshTokenSimulation.scala
@@ -1,11 +1,7 @@
 package com.linagora.openpaas.gatling
 
 import com.linagora.openpaas.gatling.Configuration._
-import com.linagora.openpaas.gatling.addressbook.ContactSteps.provisionContacts
-import com.linagora.openpaas.gatling.addressbook.scenari.OpenContactInCollectedAddressBookScenari
-import com.linagora.openpaas.gatling.calendar.EventSteps.provisionEvents
-import com.linagora.openpaas.gatling.calendar.scenari.SearchEventsScenari
-import com.linagora.openpaas.gatling.core.LoginSteps._
+import com.linagora.openpaas.gatling.core.LoginSteps.{login, logout}
 import com.linagora.openpaas.gatling.core.UserSteps.getProfile
 import com.linagora.openpaas.gatling.core.authentication.pkce.PKCESteps
 import com.linagora.openpaas.gatling.unifiedinbox.scenari.InboxScenari
@@ -13,25 +9,16 @@ import io.gatling.core.Predef._
 
 import scala.concurrent.duration.DurationInt
 
-class PlatformTestSimulation extends Simulation {
+class InboxWithRefreshTokenSimulation extends Simulation {
   private val feeder = csv("users.csv")
 
   val scn = scenario("Testing OpenPaaS mix scenarios")
     .feed(feeder.circular())
-    .pause(1 second)
     .exec(login)
     .exec(getProfile())
-    .pause(1 second)
-    .exec(provisionEvents)
-    .pause(1 second)
-    .exec(provisionContacts)
-    .pause(1 second)
     .exec(logout)
-    .pause(1 second)
     .during(ScenarioDuration) {
-      randomSwitch(
-        33.3 -> exec(SearchEventsScenari.generate()),
-        33.3 -> group("INBOX")(
+        group("INBOX")(
           exec(InboxScenari.userLogin())
             .exec(repeat(20) {
               exec(PKCESteps.renewAccessToken)
@@ -39,9 +26,8 @@ class PlatformTestSimulation extends Simulation {
                   10.0 -> exec(InboxScenari.generateOnceAlreadyLogged()),
                   90.0 -> exec(InboxScenari.idle())
                 )
-            }.exec(InboxScenari.userLogout()))),
-        33.3 -> exec(OpenContactInCollectedAddressBookScenari.generate())
-      ).pause(7500 milliseconds, 15 seconds)
+            }.exec(InboxScenari.userLogout()))
+        ).pause(7500 milliseconds, 15 seconds)
     }
 
   setUp(scn.inject(rampUsers(UserCount) during(InjectDuration))).protocols(HttpProtocol)
